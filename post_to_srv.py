3863ad06411ba844ac27f3a3ceb8df9d5c8fdd13
boer <boer0924@gmail.com>
2017-08-14 11:07:07 +0800

    exception try & add gitignore

--- post_to_srv.py
+++ post_to_srv.py
@@ -14,13 +14,28 @@ from host_performence import get_network_traffic
 
 class MyDaemon(DaemonBase):
     """Real Daemon class"""
+
+    def __init__(self,
+                 url,
+                 pidfile,
+                 stdin='/dev/null',
+                 stdout='/dev/null',
+                 stderr='/dev/null'):
+        self.url = url
+        super().__init__(pidfile, stdin, stdout, stderr)
+
     def do_post(self, params):
         data = json.dumps(params)
         headers = {'Content-Type': 'application/json'}
-        url = 'http://127.0.0.1:5000/monitor'
-        req = request.Request(url, data.encode('utf-8'), headers=headers)
-        resp = request.urlopen(req, timeout=3)
-        return resp.code
+        # url = 'http://127.0.0.1:5000/monitor'
+        req = request.Request(self.url, data.encode('utf-8'), headers=headers)
+        try:
+            resp = request.urlopen(req, timeout=3)
+        except Exception as e:
+            f = open('/tmp/test_daemon.err', 'a')
+            print('%s at: %s' % (e, time.ctime()), file=f)
+            f.close()
+        # return resp.code
 
     def run(self):
         sys.stdout.write('Daemon started with pid %s\n' % os.getpid())
@@ -41,6 +56,7 @@ if __name__ == '__main__':
         print('Usage: %s [start|stop|restart]' % sys.argv[0], file=sys.stderr)
         raise SystemExit(1)
     md = MyDaemon(
+        'http://127.0.0.1:5000/monitor',
         '/tmp/test_mydaemon.pid',
         stdout='/tmp/test_daemon.log',
         stderr='/tmp/test_daemon.log')
